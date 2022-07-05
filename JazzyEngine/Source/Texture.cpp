2687f9700def46bdcda2a456fb6c994701c9e9ee
acchang24 <acchang24@gmail.com>
2022-07-04 18:32:31 -0700

    Added DirectXTK

--- JazzyEngine/Source/Texture.cpp
+++ JazzyEngine/Source/Texture.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 #include "Texture.h"
 #include "Graphics.h"
-#include "WICTextureLoader.h"
 #include "AssetManager.h"
+#include "DirectXTK\Inc\DDSTextureLoader.h"
+#include "DirectXTK\Inc\WICTextureLoader.h"
 
 Texture::Texture()
 	: mResource(nullptr)
@@ -46,12 +47,11 @@ bool Texture::Load(const WCHAR* filename)
 	// Check the file extention of the image
 	std::wstring extention = fileStr.substr(fileStr.find_last_of('.'));
 	HRESULT hr = E_FAIL;
-	hr = DirectX::CreateWICTextureFromFile(dev, filename, &mResource, &mView);
-	//if (extention == L".dds" || extention == L".DDS")
-	//	// Create from dds file
-	//	hr = DirectX::CreateDDSTextureFromFile(dev, filename, &mResource, &mView);
-	//else
-	//	hr = DirectX::CreateWICTextureFromFile(dev, filename, &mResource, &mView);
+	if (extention == L".dds" || extention == L".DDS")
+		// Create from dds file
+		hr = DirectX::CreateDDSTextureFromFile(dev, filename, &mResource, &mView);
+	else
+		hr = DirectX::CreateWICTextureFromFile(dev, filename, &mResource, &mView);
 	DbgAssert(hr == S_OK, "Could not create a texture from file");
 	if (hr != S_OK)
 		return false;
@@ -67,7 +67,7 @@ bool Texture::Load(const WCHAR* filename)
 Texture* Texture::StaticLoad(std::string fileName, AssetManager* pManager)
 {
 	Texture* texture = new Texture();
-	
+
 	std::wstring name = std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>{}.from_bytes(fileName.data());
 
 	if (!texture->Load(name.c_str()))
--- Tools/DirectXTK/Inc/WICTextureLoader.h
+++ Tools/DirectXTK/Inc/WICTextureLoader.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------
-// File: WICTextureLoader11.h
+// File: WICTextureLoader.h
 //
 // Function for loading a WIC image and creating a Direct3D runtime texture for it
 // (auto-generating mipmaps if possible)
@@ -14,8 +14,12 @@
 // For a full-featured DDS file reader, writer, and texture processing pipeline see
 // the 'Texconv' sample and the 'DirectXTex' library.
 //
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
 //
 // http://go.microsoft.com/fwlink/?LinkId=248926
 // http://go.microsoft.com/fwlink/?LinkId=248929
@@ -23,77 +27,70 @@
 
 #pragma once
 
+#if defined(_XBOX_ONE) && defined(_TITLE)
+#include <d3d11_x.h>
+#else
 #include <d3d11_1.h>
+#endif
 
-#pragma comment(lib,"dxguid.lib")
-#pragma comment(lib,"windowscodecs.lib")
+#include <stdint.h>
 
-#include <cstddef>
-#include <cstdint>
 
 namespace DirectX
 {
-#ifndef WIC_LOADER_FLAGS_DEFINED
-#define WIC_LOADER_FLAGS_DEFINED
-    enum WIC_LOADER_FLAGS : uint32_t
+    enum WIC_LOADER_FLAGS
     {
-        WIC_LOADER_DEFAULT = 0,
-        WIC_LOADER_FORCE_SRGB = 0x1,
-        WIC_LOADER_IGNORE_SRGB = 0x2,
-        WIC_LOADER_SRGB_DEFAULT = 0x4,
-        WIC_LOADER_FIT_POW2 = 0x20,
-        WIC_LOADER_MAKE_SQUARE = 0x40,
-        WIC_LOADER_FORCE_RGBA32 = 0x80,
+        WIC_LOADER_DEFAULT      = 0,
+        WIC_LOADER_FORCE_SRGB   = 0x1,
+        WIC_LOADER_IGNORE_SRGB  = 0x2,
     };
 
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-dynamic-exception-spec"
-#endif
-
-    DEFINE_ENUM_FLAG_OPERATORS(WIC_LOADER_FLAGS);
-
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
-#endif
-
     // Standard version
-    HRESULT CreateWICTextureFromMemory(
+    HRESULT __cdecl CreateWICTextureFromMemory(
         _In_ ID3D11Device* d3dDevice,
         _In_reads_bytes_(wicDataSize) const uint8_t* wicData,
         _In_ size_t wicDataSize,
         _Outptr_opt_ ID3D11Resource** texture,
         _Outptr_opt_ ID3D11ShaderResourceView** textureView,
-        _In_ size_t maxsize = 0) noexcept;
+        _In_ size_t maxsize = 0);
 
-    HRESULT CreateWICTextureFromFile(
+    HRESULT __cdecl CreateWICTextureFromFile(
         _In_ ID3D11Device* d3dDevice,
         _In_z_ const wchar_t* szFileName,
         _Outptr_opt_ ID3D11Resource** texture,
         _Outptr_opt_ ID3D11ShaderResourceView** textureView,
-        _In_ size_t maxsize = 0) noexcept;
+        _In_ size_t maxsize = 0);
 
     // Standard version with optional auto-gen mipmap support
-    HRESULT CreateWICTextureFromMemory(
+    HRESULT __cdecl CreateWICTextureFromMemory(
+    #if defined(_XBOX_ONE) && defined(_TITLE)
+        _In_ ID3D11DeviceX* d3dDevice,
+        _In_opt_ ID3D11DeviceContextX* d3dContext,
+    #else
         _In_ ID3D11Device* d3dDevice,
         _In_opt_ ID3D11DeviceContext* d3dContext,
+    #endif
         _In_reads_bytes_(wicDataSize) const uint8_t* wicData,
         _In_ size_t wicDataSize,
         _Outptr_opt_ ID3D11Resource** texture,
         _Outptr_opt_ ID3D11ShaderResourceView** textureView,
-        _In_ size_t maxsize = 0) noexcept;
+        _In_ size_t maxsize = 0);
 
-    HRESULT CreateWICTextureFromFile(
+    HRESULT __cdecl CreateWICTextureFromFile(
+    #if defined(_XBOX_ONE) && defined(_TITLE)
+        _In_ ID3D11DeviceX* d3dDevice,
+        _In_opt_ ID3D11DeviceContextX* d3dContext,
+    #else
         _In_ ID3D11Device* d3dDevice,
         _In_opt_ ID3D11DeviceContext* d3dContext,
+    #endif
         _In_z_ const wchar_t* szFileName,
         _Outptr_opt_ ID3D11Resource** texture,
         _Outptr_opt_ ID3D11ShaderResourceView** textureView,
-        _In_ size_t maxsize = 0) noexcept;
+        _In_ size_t maxsize = 0);
 
     // Extended version
-    HRESULT CreateWICTextureFromMemoryEx(
+    HRESULT __cdecl CreateWICTextureFromMemoryEx(
         _In_ ID3D11Device* d3dDevice,
         _In_reads_bytes_(wicDataSize) const uint8_t* wicData,
         _In_ size_t wicDataSize,
@@ -102,11 +99,11 @@ namespace DirectX
         _In_ unsigned int bindFlags,
         _In_ unsigned int cpuAccessFlags,
         _In_ unsigned int miscFlags,
-        _In_ WIC_LOADER_FLAGS loadFlags,
+        _In_ unsigned int loadFlags,
         _Outptr_opt_ ID3D11Resource** texture,
-        _Outptr_opt_ ID3D11ShaderResourceView** textureView) noexcept;
+        _Outptr_opt_ ID3D11ShaderResourceView** textureView);
 
-    HRESULT CreateWICTextureFromFileEx(
+    HRESULT __cdecl CreateWICTextureFromFileEx(
         _In_ ID3D11Device* d3dDevice,
         _In_z_ const wchar_t* szFileName,
         _In_ size_t maxsize,
@@ -114,14 +111,19 @@ namespace DirectX
         _In_ unsigned int bindFlags,
         _In_ unsigned int cpuAccessFlags,
         _In_ unsigned int miscFlags,
-        _In_ WIC_LOADER_FLAGS loadFlags,
+        _In_ unsigned int loadFlags,
         _Outptr_opt_ ID3D11Resource** texture,
-        _Outptr_opt_ ID3D11ShaderResourceView** textureView) noexcept;
+        _Outptr_opt_ ID3D11ShaderResourceView** textureView);
 
     // Extended version with optional auto-gen mipmap support
-    HRESULT CreateWICTextureFromMemoryEx(
+    HRESULT __cdecl CreateWICTextureFromMemoryEx(
+#if defined(_XBOX_ONE) && defined(_TITLE)
+        _In_ ID3D11DeviceX* d3dDevice,
+        _In_opt_ ID3D11DeviceContextX* d3dContext,
+    #else
         _In_ ID3D11Device* d3dDevice,
         _In_opt_ ID3D11DeviceContext* d3dContext,
+    #endif
         _In_reads_bytes_(wicDataSize) const uint8_t* wicData,
         _In_ size_t wicDataSize,
         _In_ size_t maxsize,
@@ -129,20 +131,25 @@ namespace DirectX
         _In_ unsigned int bindFlags,
         _In_ unsigned int cpuAccessFlags,
         _In_ unsigned int miscFlags,
-        _In_ WIC_LOADER_FLAGS loadFlags,
+        _In_ unsigned int loadFlags,
         _Outptr_opt_ ID3D11Resource** texture,
-        _Outptr_opt_ ID3D11ShaderResourceView** textureView) noexcept;
+        _Outptr_opt_ ID3D11ShaderResourceView** textureView);
 
-    HRESULT CreateWICTextureFromFileEx(
+    HRESULT __cdecl CreateWICTextureFromFileEx(
+    #if defined(_XBOX_ONE) && defined(_TITLE)
+        _In_ ID3D11DeviceX* d3dDevice,
+        _In_opt_ ID3D11DeviceContextX* d3dContext,
+    #else
         _In_ ID3D11Device* d3dDevice,
         _In_opt_ ID3D11DeviceContext* d3dContext,
+    #endif
         _In_z_ const wchar_t* szFileName,
         _In_ size_t maxsize,
         _In_ D3D11_USAGE usage,
         _In_ unsigned int bindFlags,
         _In_ unsigned int cpuAccessFlags,
         _In_ unsigned int miscFlags,
-        _In_ WIC_LOADER_FLAGS loadFlags,
+        _In_ unsigned int loadFlags,
         _Outptr_opt_ ID3D11Resource** texture,
-        _Outptr_opt_ ID3D11ShaderResourceView** textureView) noexcept;
+        _Outptr_opt_ ID3D11ShaderResourceView** textureView);
 }
